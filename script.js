48c705061a1ba3e70896afa09b673704420a1344
shatrudhan kumar <168300232+shatrudhank21@users.noreply.github.com>
2024-09-06 20:30:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//change navbar style
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll',window.scrollY>0)
+})
+
+//show hide faq answer
+const faqs=document.querySelectorAll('.faq');
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon=faq.querySelector('.faq-icon i');
+        if(icon.className==='uil uil-plus'){
+            icon.className="uil uil-minus"
+        }
+        else{
+            icon.className="uil uil-plus";
+        }
+    })
+})
+
+
+//show/hide
+const menu=document.querySelector(".nav_menu");
+const menubtn=document.querySelector("#open-menu-btn");
+const closebtn=document.querySelector("#close-menu-btn");
+
+
+menubtn.addEventListener("click",()=>{
+    menu.style.display="flex";
+    closebtn.style.display="inline-block";
+    menubtn.style.display="none";
+})
+
+const closenav=()=>{
+    menu.style.display="none";
+    closebtn.style.display="none";
+    menubtn.style.display="inline-block";
+}
+closebtn.addEventListener("click",closenav);
